b7873d07d87011520f5bd7693ee64303665ccebb
kokole12 <kokoleismail886@gmail.com>
2023-03-04 13:50:43 +0300

    working on the position

--- Algorithms/4-algo.py
+++ Algorithms/4-algo.py
@@ -16,13 +16,10 @@
 """
 def count_rotations_linear(nums):
     
-    position = 0
+    position = 1
 
     while position < len(nums):
-
         if position > 0 and nums[position] < nums[position - 1]:
             return position
-        
         position = position + 1
-
-    return 0
+        return 0
