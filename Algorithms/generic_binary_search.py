c54347673ed3c52cc6a0f2bb33c145e0574b2c6d
kokole12 <kokoleismail886@gmail.com>
2023-03-04 12:32:23 +0300

    pycodestyle

--- Algorithms/generic_binary_search.py
+++ Algorithms/generic_binary_search.py
@@ -5,7 +5,7 @@ def binary_search(left, right, condition):
         if condition(mid) == 'found':
             return mid
         elif condition(mid) == 'left':
-            left =  mid -1
+            left = mid - 1
         else:
             right = mid + 1
     return -1
