7b987f2b93f8843449db6b23f24afab774c8b209
kokole12 <kokoleismail886@gmail.com>
2023-03-04 13:32:49 +0300

    added comments to explain code

--- Algorithms/4-algo.py
+++ Algorithms/4-algo.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
+
+""" problem is to find the number of rotations in a rotated sorted list
+    the complexity has to be 0(logn)
+
+    clear problem statement is:
+    write a function that takes nums as an input and determines the position
+    of the smallest element since each ration take the element to a position equal to 
+    the number of rotations
+
+    steps:
+    1. define the function and initialise a variable called position=1
+    2. check if the value at the position is less than the predicessor
+    3. if true, return the position as number of rotations
+    4. else the lise has not been rotated and return 0
+"""
 def count_rotations_linear(nums):
     
-    position = 1
+    position = 0
 
     while position > len(nums):
 
--- Algorithms/4-test.py
+++ Algorithms/4-test.py
@@ -3,9 +3,9 @@ count_rotations_linear = __import__('4-algo').count_rotations_linear
 
 if __name__ == '__main__':
     from pprint import pprint
-    test = []
+    tests = []
     """ a list of 10 items rotated 3 times"""
-    test.append({
+    tests.append({
         'input':{
             'nums': [10, 9, 8, 1, 2, 3, 4, 5, 6, 7]
         },
@@ -14,11 +14,12 @@ if __name__ == '__main__':
 
 
     """ A list of 8 rotated 5 times"""
-    test.append({
+    tests.append({
         'input':{
             'nums': [9, 8, 7, 6, 5, 1, 3, 4]
         }, 
         'output': 5
     })
-
-    pprint(test)
+    test = tests[1]
+    print(count_rotations_linear(test['input']['nums'])== test['output'])
+    pprint(tests)
