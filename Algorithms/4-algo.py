09f8662474d458bbaebcddbb60e0c6b8bf9b1c53
kokole12 <kokoleismail886@gmail.com>
2023-03-04 13:13:50 +0300

    tests

---
+++ Algorithms/4-algo.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def count_rotations(nums):
+    
+    position = 1
+
+    while position > len(nums):
+
+        if position > 0 and nums[position] < nums[position - 1]:
+            return position
+        
+        position += 1
+
+    return 0
