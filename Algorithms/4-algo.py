de97851bd6fdd7d72750291f87b3e99cba893a6d
kokole12 <kokoleismail886@gmail.com>
2023-03-04 13:48:48 +0300

    fixing baug

--- Algorithms/4-algo.py
+++ Algorithms/4-algo.py
@@ -18,11 +18,11 @@ def count_rotations_linear(nums):
     
     position = 0
 
-    while position > len(nums):
+    while position < len(nums):
 
         if position > 0 and nums[position] < nums[position - 1]:
             return position
         
-        position += 1
+        position = position + 1
 
     return 0
