f5013e5a4cdf34b68ed9eb14fabd44c686a50345
kokole12 <kokoleismail886@gmail.com>
2023-03-06 12:04:15 +0300

    dll

--- Algorithms/data_strcutures_python/0-dll.py
+++ Algorithms/data_strcutures_python/0-dll.py
@@ -36,6 +36,30 @@ class Doubly_linked_list:
         last_node.next = node
         node.prev =  last_node
 
+    def get_length(self):
+        current_node = self.head
+        count = 0
+
+        while current_node:
+            count += 1
+            current_node = current_node.next
+        return count
+    
+        
+    def insertAtPosition(self, data, index):
+        current_node = self.head
+        count = 0
+
+        while current_node:
+            if  count == index - 1:
+                node = Node(data, current_node.next)
+                current_node.prev = node
+                break
+
+            current_node = current_node.next
+            node.prev = current_node 
+            count += 1
+
     def print_dll(self):
         if self.head is None:
             print('empty list')
@@ -60,3 +84,5 @@ dlls.insetAtBeginning(10)
 dlls.insertAtEnd(20)
 dlls.insertAtEnd(40)
 dlls.print_dll()
+length = dlls.get_length()
+print(length)
