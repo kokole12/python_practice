7d99bb4dc6d0aa67b0098ab6fc88f69d12245d40
kokole12 <kokoleismail886@gmail.com>
2023-03-06 10:56:59 +0300

    doubly linked list

---
+++ Algorithms/data_strcutures_python/0-dll.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+class Node:
+    
+    def __init__(self, data=None, next=None, prev=None):
+        self.data = data
+        self.next = next
+        self.prev = prev
+
+
+class Doubly_linked_list:
+    def __init__(self):
+        self.head = None
+    
+
+    def insetAtBeginning(self, data):
+        node = Node(data, self.head)
+        node.prev = None
+
+        if self.head is not None:
+            self.head.prev = node
+        
+        self.head= node
+
+    def print_dll(self):
+        if self.head is None:
+            print('empty list')
+            return
+        
+        current_node  = self.head
+        dlls = ''
+        while current_node:
+            dlls += str(current_node.data) + '-->'
+            current_node = current_node.next
+        
+        print(dlls)
+
+
+
+dlls = Doubly_linked_list()
+
+dlls.insetAtBeginning(3)
+dlls.insetAtBeginning(4)
+dlls.insetAtBeginning(10)
+dlls.print_dll()
