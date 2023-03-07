f42a852a2cb76bf257f58bb976981e631941ba71
kokole12 <kokoleismail886@gmail.com>
2023-03-07 11:43:36 +0300

    binary tree

---
+++ python_binary_tree/0-bst.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+class Node:
+    def __init__(self, data=None, leftChild=None, rightChild=None):
+        self.data = data
+        self.leftChild = leftChild
+        self.rightChild = rightChild
