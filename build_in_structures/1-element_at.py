8fc6cbc23e2ef56fcd9543ee909ba1155b8224f5
kokole12 <kokoleismail886@gmail.com>
2023-03-09 14:33:38 +0300

    code with complexity of 0(1)

--- build_in_structures/1-element_at.py
+++ build_in_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if idx < 0:
+    """if idx < 0:
         return None
     if idx >= len(my_list):
         return None
@@ -9,3 +9,10 @@ def element_at(my_list, idx):
     for i in range(0, len(my_list)):
         if i == idx:
             return my_list[i]
+    """
+
+    if idx < 0  or idx >= len(my_list):
+        return None
+    else:
+        return my_list[idx]
+
