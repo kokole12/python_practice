1209d5ddf96bfa3669bcd3befc06572392a0d07c
kokole12 <kokoleismail886@gmail.com>
2023-03-09 11:42:38 +0300

    add function import

---
+++ import_modules/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from add_0 import add
+    a = 2
+    b = 3
+
+    result = add(a, b)
+
+    print('{} + {} = {}'.format(a, b, result))
---
+++ import_modules/add_0.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def add(a, b):
+    """
+        my addition  function
+
+        Args:
+            a: first argument
+            b: second argument
+        
+        Returns:
+            The value of a + b
+        
+        
+        return (a + b)
+    """
+    return (a + b)
