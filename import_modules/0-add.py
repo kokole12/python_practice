88af6648d4e0e3f806f7c1e7986a9fe5f0ee1455
kokole12 <kokoleismail886@gmail.com>
2023-03-09 11:45:18 +0300

    function

--- import_modules/0-add.py
+++ import_modules/0-add.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
+    """ 
+        importing the add_0 module
+
+        args:
+            a = 2
+            b = 3
+        
+        Returns:
+            The value of a + b
+        
+        add(a, b)
+    """
     from add_0 import add
     a = 2
     b = 3
