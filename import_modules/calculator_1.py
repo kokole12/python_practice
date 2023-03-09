a3360522bfdc2adc4db5462d40db2d46f0421494
kokole12 <kokoleismail886@gmail.com>
2023-03-09 12:00:10 +0300

    calculator

---
+++ import_modules/calculator_1.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+def add(a, b):
+    """
+        My addition function
+
+        args:
+            a: first argument
+            b: second argument
+        
+        Returns:
+            The value of a + b
+        
+        return (a + b)
+    """
+    return (a + b)
+
+def sub(a, b):
+    """
+        My subtraction function
+
+        Args:
+            a: first argument
+            b: second argument
+        
+        Returns:
+            The value of a - b
+        
+        return (a - b)
+    """
+    return (a - b)
+
+def div(a, b):
+    """
+        My division function
+
+        Args:
+            a: first arg
+            b:  second arg
+        
+        Return:
+            The value of a / b
+
+        return a / b
+    """
+    return (a /b )
+
+def mul(a, b):
+    """
+        My multiplication function
+
+        Args:
+            a: first arg
+            b: second arg
+        Returns:
+            the value of a * b
+        return a *  b
+    """
+    return (a * b)
