053dfa4bfc69703681e8d0cc458597618f878d07
kokole12 <kokoleismail886@gmail.com>
2023-03-09 14:32:02 +0300

    code with time complexity of 0(n)

---
+++ build_in_structures/1-element_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    if idx >= len(my_list):
+        return None
+        
+    for i in range(0, len(my_list)):
+        if i == idx:
+            return my_list[i]
---
+++ build_in_structures/1-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+element_at = __import__('1-element_at').element_at
+
+my_list = [1, 2, 3, 4, 5, 6, 7]
+idx = 8
+
+print(element_at(my_list, idx))
