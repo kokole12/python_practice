34350e58b46c8a7c7fa0fb0b1b1ffb88dbf658c0
kokole12 <kokoleismail886@gmail.com>
2023-03-11 12:40:08 +0300

    using dictionary comprehention

--- more_python_data_strcutures/9-multiply_by_2.py
+++ more_python_data_strcutures/9-multiply_by_2.py
@@ -4,9 +4,11 @@ def multiply_by_2(a_dictionary):
     for key, val in a_dictionary.items():
         return {key: val * 2}
 
-    # return {key: val * 2 for key, val in a_dictionary.items()}
+    
 
+    """ 
+        Using a dictinary comprehension
+        return {key: val * 2 for key, val in a_dictionary.items()}
 
-    """ Using a for loop
     """
     
