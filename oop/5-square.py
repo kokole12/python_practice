7cd819ca2e0a90b936aac8ef470f05c70976eb19
kokole12 <kokoleismail886@gmail.com>
2023-03-12 16:19:13 +0300

    function for printitng square

---
+++ oop/5-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("Size must be an integer")
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """ returns the value of size"""
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        """ sets the value for size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+        
+        str = '#'
+        for x in range(self.__size):
+            print(str * self.__size)
